56290ef7b1c9a9f0ca1cb83485f5a935e4e39f14
Jay <patiljay956@gmail.com>
2023-12-26 22:58:20 +0530

    minor bug fix and gitinore edit

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,5 +1,5 @@
 class ApiResponse {
-    constructor(statusCode, data, message = "sucess") {
+    constructor(statusCode, data, message = "success") {
         this.statusCode = statusCode;
         this.data = data;
         this.message = message;
