a487d61818b437b5780e310e38bfa05d72c6e7c1
Jay <patiljay956@gmail.com>
2023-12-30 18:58:01 +0530

    simple user controller

--- src/app.js
+++ src/app.js
@@ -4,23 +4,25 @@ import cors from "cors";
 
 const app = express();
 
-// Configurations 
+// Configurations
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(express.json({ limit: "16kb" }));
 app.use(cookieParser());
 
+// routes import
+import userRouter from "./routes/user.routes.js";
 
+// route declaration
+app.use("/api/v1/users", userRouter);
 
-
-
-
-
-
+// http;//localhost:3000/api/v1/users/register
 export default app;
